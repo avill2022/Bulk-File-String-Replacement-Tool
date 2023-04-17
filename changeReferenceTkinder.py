cc03c62fcb94022b5a22f8d37a195e322f09afc7
avill <avill.vg@gmail.com>
2023-04-17 12:21:13 -0600

    change the structure

--- changeReferenceTkinder.py
+++ changeReferenceTkinder.py
@@ -1,88 +1,191 @@
+import customtkinter
 import tkinter as tk
-from tkinter import *
-
-import os
-from pathlib import *
 from tkinter import filedialog
 from tkinter import messagebox
-from tkinter import scrolledtext
+import os
+import shutil
 
-class FolderSelectGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Change reference")
-        master.geometry('350x280')
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  
 
-        self.folder_path = tk.StringVar()
+build_gradle_project = ""
+build_gradle_app = ""
+folder_path=""
+count_files = 0
 
-        self.label = tk.Label(master, textvariable=self.folder_path)
-        self.label.pack(fill=X)
+to_folder="\\project"
 
-        self.select_button = tk.Button(master, text="Select Folder", command=lambda:select_folder())
-        self.select_button.pack(fill=X)
+def howManyFiles():
+    global count_files
+    global folder_path 
+    if(folder_path):
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                dire = os.path.join(root, file)
+                if file.endswith(".kt") or file.endswith(".xml") or file.endswith(".java") or file.endswith(".gradle"):
+                        count_files +=1
+    return count_files
 
-        #frame 1
-        self.frame1 = Frame(master=self.master, width=200, height=100)
-        self.frame1.pack(fill=X)
-        #elements textFrom ...
-        self.lbl = Label(self.frame1, text="change")
-        self.lbl.grid(column=0, row=0)
-        self.textFrom = Entry(self.frame1,width=48)
-        self.textFrom.grid(column=1, row=0)
 
-        #frame 2
-        self.frame2 = Frame(master=self.master, width=100)
-        self.frame2.pack(fill=X)
-        #elements to ...
-        self.lb2 = Label(self.frame2, text="to")
-        self.lb2.grid(column=0, row=0)
-        self.textTo = Entry(self.frame2,width=50)
-        self.textTo.grid(column=1, row=0)
 
-        self.frame3 = Frame(master=self.master, width=50)
-        self.frame3.pack(fill=X)
-        self.txt3 = scrolledtext.ScrolledText(self.frame3,width=40,height=10)
-        self.txt3.grid(column=0,row=0)
+def copyFile(file_url):
+    #os.walk(os.getcwd())
+    global to_folder
+    print(file_url)
+    dst_file_path = "app\src\main\java"+"\\"+file_url.split("\\")[5]+"\\"+file_url.split("\\")[6]+"\\"+file_url.split("\\")[7]    
+    folder = file_url.split(dst_file_path)[1]
+    #extract the file of the url
+    a = folder.split("\\")[-1]
+    #take the size of the name
+    size = len(a)
+    #take the first part
+    my_string = folder[:-size]
+    #current direction folder
+    script_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(script_path)
+    folder_destiny = script_dir +to_folder+"\\app\\main"+ my_string
 
-        btn = Button(self.frame3, text="Start",command=lambda:start_scan())
-        btn.grid(column=0, row=1)
+    if not os.path.exists(folder_destiny):
+        os.makedirs(folder_destiny)
 
-        def select_folder():
-            folder_path = filedialog.askdirectory()
-            self.folder_path.set(folder_path)
+    file_destiny =  folder_destiny + a
 
-        def changeFile(campo):
-            file=open(campo,'r+')
-            l=file.readlines()
-            result=[]
-            for i in l:
-                result.append(i.replace(self.textFrom.get(),self.textTo.get()))
-            file.close()
+    #copy the file
+    shutil.copy(file_url, file_destiny)
 
-            file=open(campo,'w')
-            file.truncate(0)
-            for a in result:
-                file.write(a)
-            file.close()
+    if os.path.exists(file_destiny):
+        count_progress=progressbar_1.get()
+        count_progress+=1
+        progressbar_1.set(count_progress)
+        print(f"The file '{file_destiny}' was copied successfully.")
+    else:
+        print(f"There was an error copying the file.")
 
-        def changeFiles():
-            #os.walk(os.getcwd())
-            for root, dirs, files in os.walk(self.folder_path.get()):
+    #change reference
+    if(textFrom.get() and textTo.get()):
+        changeFile(file_destiny)
+        os.rename(file_destiny, file_destiny.replace(textFrom.get(),textTo.get()))
+
+def changeFile(campo):
+    file=open(campo,'r+')
+    l=file.readlines()
+    result=[]
+    for i in l:
+        result.append(i.replace(textFrom.get(),textTo.get()))
+    file.close()
+
+    file=open(campo,'w')
+    file.truncate(0)
+    for a in result:
+        file.write(a)
+    file.close()
+
+def start_cleaning():
+    global folder_path
+    global to_folder
+
+    if(folder_path):
+        #current direction folder
+        script_path = os.path.abspath(__file__)
+        folderDestiny = os.path.dirname(script_path) + to_folder
+
+        #copy the gradle
+        f1 = folder_path+"/build.gradle"
+        f2 = folderDestiny+"/build.gradle"
+
+        if not os.path.exists(folderDestiny+"/app/"):
+            os.makedirs(folderDestiny+"/app/")
+
+        shutil.copy(f1, f2)
+        #copy the gradle #2
+        f1 = folder_path+"/app/build.gradle"
+        f2 = folderDestiny+"/app/build.gradle"
+
+        if not os.path.exists(folderDestiny+"/app/"):
+            os.makedirs(folderDestiny+"/app/")
+        shutil.copy(f1, f2)
+        #copy the manifest 
+        if not os.path.exists(folderDestiny+"\\app"):
+            os.makedirs(folderDestiny+"\\app")
+        shutil.copy(folder_path+"\\app\src\main\AndroidManifest.xml", folderDestiny+"\\app\AndroidManifest.xml")
+        #copy the res folder
+        # remove the destination folder if it already exists
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        if os.path.exists(folderDestiny+"\\app\\res"):
+            shutil.rmtree(folderDestiny+"\\app\\res")
+        shutil.copytree(folder_path+"\\app\\src\\main\\res", folderDestiny+"\\app\\res")
+
+        print("Starting cleaning")
+        if(folder_path):
+            for root, dirs, files in os.walk(folder_path):
                 for file in files:
+                    dire = os.path.join(root, file)
                     if file.endswith(".kt") or file.endswith(".xml") or file.endswith(".java"):
-                        changeFile(os.path.join(root, file))
-                        os.rename(os.path.join(root, file), os.path.join(root, file).replace(self.textFrom.get(),self.textTo.get()))
-                        self.txt3.insert(INSERT,os.path.join(root, file)+'\n')
+                        if "app\src\main\java" in dire:
+                            copyFile(os.path.join(root, file))
+        messagebox.showinfo("####SUCCESS#####", "All files are clean!")
+    else:
+        messagebox.showinfo("There isn't a folder selected!", "Select a android project")
 
-        def start_scan():
-            if self.label.cget("text"):
-                if self.textFrom.get() and self.textTo.get():
-                    changeFiles()
-                else:
-                    messagebox.showinfo("I need the reference", "WARNING! All the files reference will be changed!")
-            else:
-                messagebox.showinfo("Select a Folder", "WARNING! All the files in the folder will be changed!")
 
-root = tk.Tk()
-my_gui = FolderSelectGUI(root)
-root.mainloop()
+def select_folder():
+    global folder_path 
+    folder_path = filedialog.askdirectory()
+    if os.path.exists(folder_path+"/gradle.properties") and os.path.exists(folder_path+"/build.gradle"):
+        print(f"The file '{folder_path}' exists.")
+        
+        build_gradle_project = folder_path+"/gradle.properties"
+        howManyFiles()
+        progressbar_1.size= howManyFiles()
+
+        entry_1.insert(0, "")
+        entry_1.insert(0, folder_path)
+    else:
+        print(f"The file '{folder_path}' does not exist.")
+        entry_1.insert(0, "")
+        entry_1.insert(0, folder_path)
+        messagebox.showinfo("This folder isn't a Gradle project", "WARNING! This folder isn't a Gradle project!")
+
+app = customtkinter.CTk()
+app.geometry("400x480")
+app.title("Change Reference Android Projects")
+
+mainWindow = customtkinter.CTkFrame(master=app)
+mainWindow.pack(pady=20, padx=20, fill="both", expand=True)
+
+titleLabel = customtkinter.CTkLabel(mainWindow, text="Clean Android Project", font=customtkinter.CTkFont(size=20, weight="bold"), justify=customtkinter.LEFT)
+titleLabel.pack(pady=10, padx=10)
+
+button_1 = customtkinter.CTkButton(master=mainWindow, text="Select folder", command=select_folder)
+button_1.pack(pady=10, padx=10, fill="both")
+
+entry_1 = customtkinter.CTkEntry(master=mainWindow, placeholder_text="C:/Users/james/Desktop/")
+entry_1.pack(pady=10, padx=10, fill="both")
+
+
+#frame 1
+frame1 = customtkinter.CTkFrame(master=mainWindow, width=200, height=100)
+frame1.pack(pady=10, padx=10, fill="both")
+#elements textFrom ...
+lbl = customtkinter.CTkLabel(frame1, text="change: ")
+lbl.grid(column=0, row=0)
+textFrom = customtkinter.CTkEntry(frame1, placeholder_text="com.something.something",width=100)
+textFrom.grid(column=1, row=0)
+
+#elements textFrom ...
+lto = customtkinter.CTkLabel(frame1, text="to: ")
+lto.grid(column=0, row=1)
+textTo = customtkinter.CTkEntry(frame1, placeholder_text="com.something.something",width=100)
+textTo.grid(column=1, row=1)
+
+progressbar_1 = customtkinter.CTkProgressBar(master=mainWindow)
+progressbar_1.pack(pady=10, padx=10,fill="both")
+progressbar_1.set(0.0)
+
+btn = customtkinter.CTkButton(mainWindow, text="Start",command=start_cleaning)
+btn.pack(pady=10, padx=10)
+
+
+app.mainloop()
